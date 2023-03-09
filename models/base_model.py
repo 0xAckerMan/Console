8a572682082fe1154a1d253a669d46a08558eed5
0xAckerMan <joelkores1@gmail.com>
2023-03-09 14:58:26 +0300

    Updated the dict kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,22 @@ class BaseModel:
     '''
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
         if len(kwargs) == 0:
-            ...
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
 
     def __str__(self):
         return (
