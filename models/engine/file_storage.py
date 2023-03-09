6c8190b0758fd15253f3e13d77f7338f5aa6325f
0xAckerMan <joelkores1@gmail.com>
2023-03-09 21:36:06 +0300

    Updated the file storage model json,dumps

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
 ''' File storage class'''
-
+import json
 
 class FileStorage:
-    ...
+    '''
+    Class implementation that serializes instance to json
+    and deserializes JSON to instances
+    '''
+    
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    
+    def all(self):
+        '''returns all objects'''
+        return self.__objects
+    
+    def new(self, obj):
+        '''
+        Sets key for object
+        args:
+            @obj
+        '''
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+    
+    def save(self):
+        '''saves to json'''
+        with open(FileStorage.__file_path, mode='w', encoding='UTF-8') as f:
+            my_obj ={}
+            for k, v in FileStorage.__objects.items():
+                my_obj[k] = v.to_dict()
+            my_str = json.dumps(my_obj)
+            f.write(my_str)
