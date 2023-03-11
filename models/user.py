839d1efc79f65e636cb39d6d911150f4fbaf5dd6
0xAckerMan <joelkores1@gmail.com>
2023-03-11 11:05:05 +0300

    fixed import error

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' creates a user from the basemodel'''
 
-from models import base_model
+from models.base_model import BaseModel
 
 
-class User(base_model):
+class User(BaseModel):
     '''Class implementation'''
 
     email = ''
