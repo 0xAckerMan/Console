5e6e9330546ae4d16ab9def78f0eabd14750d25a
0xAckerMan <joelkores1@gmail.com>
2023-03-12 21:10:01 +0300

    Added the create func"

--- console.py
+++ console.py
@@ -15,19 +15,39 @@ class HBNBCommand(cmd.Cmd):
     '''
 
     intro = '''
+**************************************************************    
  _____ _   _ _____    ____ ___  _   _ ____   ___  _     _____ 
 |_   _| | | | ____|  / ___/ _ \| \ | / ___| / _ \| |   | ____|
   | | | |_| |  _|   | |  | | | |  \| \___ \| | | | |   |  _|  
   | | |  _  | |___  | |__| |_| | |\  |___) | |_| | |___| |___ 
   |_| |_| |_|_____|  \____\___/|_| \_|____/ \___/|_____|_____|
-                                                              
 
+***************************************************************
     '''
     prompt = '(hbnb)'
 
     def do_quit(self, line):
         '''Exiting the console'''
         return True
+    
+    def do_create(self, line):
+        """
+        Create an instance of BaseModel
+        Usage: create <class Name>
+        """
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif (HBNBCommand.check_class(args[0])):
+            if not issubclass(eval(args[0]), BaseModel):
+                print("** class doesn't exist **")
+            else:
+                my_class = eval(args[0])()
+                my_class.save()
+                print("{}".format(my_class.id))
+        else:
+            print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
